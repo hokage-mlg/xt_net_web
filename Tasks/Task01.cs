ced5015cef79acdfdd3d0a5e303d2b7465ebb71a
Nikita Vasin(notebook) <nikitavsn64rus@gmail.com>
2019-11-16 19:31:32 +0400

    Clear repo

---
+++ Tasks/Task01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Tasks0;
+
+namespace TasksVasin
+{
+    class Task01
+    {
+        public static List<int> Sequence(int N)
+        {
+            List<int> primes = new List<int>();
+            for (int i = 1; i <= N; i++)
+                primes.Add(i);
+            return primes;
+        }
+        public static void SequenceDisplay(int N)
+        {
+            if (N == 0)
+                Console.WriteLine("N>0!");
+            Console.WriteLine("{0}\n", String.Join(",", Sequence(N)));
+        }
+    }
+}
